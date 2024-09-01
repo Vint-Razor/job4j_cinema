674bae8be2114e9bb5a17d81a7d1c3efab42c519
Vint-Razor <vint-razor@yandex.ru>
2024-09-01 22:10:12 +0500

    Подключил статические bootstrap, jquery

--- src/main/java/ru/job4j/cinema/controller/IndexController.java
+++ src/main/java/ru/job4j/cinema/controller/IndexController.java
@@ -1,13 +1,13 @@
 package ru.job4j.cinema.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class IndexController {
 
     @GetMapping("/index")
     public String getIndex() {
-        return "Hello Cinema";
+        return "index";
     }
 }
